8d162a98a97aeb5712dde86fc4fdccd49e0fe6a5
amant2904 <tiwariaman2904@gmail.com>
2023-03-15 12:55:43 +0530

    only add to cart button working now

--- src/Components/Store/CartProvider.js
+++ src/Components/Store/CartProvider.js
@@ -3,18 +3,18 @@ import CartContext from './cart-context'
 import StockContext from './stock-context'
 
 const cartItemReducer = (state, action) => {
-    if (action.type === "addInCartList") {
-        return {
-            items: [...state.items, action.itemDetail]
-        }
-    }
+    // if (action.type === "addInCartList") {
+    //     return {
+    //         items: [...state.items, action.itemDetail]
+    //     }
+    // }
     return {
         items: []
     }
 }
 
 export default function CartProvider(props) {
-    const stkCtx = useContext(StockContext);
+    // const stkCtx = useContext(StockContext);
 
     const [cartItems, dispatchCartItems] = useReducer(cartItemReducer, {
         items: [{
@@ -24,9 +24,9 @@ export default function CartProvider(props) {
         }]
     })
 
-    if (stkCtx.cartItem !== null) {
-        dispatchCartItems({ type: "addInCartList", itemDetail: stkCtx.cartItem });
-    }
+    // if (stkCtx.cartItem !== null) {
+    //     dispatchCartItems({ type: "addInCartList", itemDetail: stkCtx.cartItem });
+    // }
 
     return (
         <CartContext.Provider value={{
